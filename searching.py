eefb01ab1da3db67a8f30846ed786eeb0e246c9a
anastasieh <272102@vutbr.cz>
2026-04-16 14:58:19 +0200

    prvni reseni

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,11 +13,19 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        if field not in data:
+            return None
+        else:
+            return data[field]
+
 
 
 def main():
+    sequential_data = read_data("sequential.json", "ordered_numbers")
     pass
-
+    print(sequential_data)
 
 if __name__ == '__main__':
     main()
