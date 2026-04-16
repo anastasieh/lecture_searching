a06d46581825b5d1698d5340dd4e72af07090054
anastasieh <272102@vutbr.cz>
2026-04-16 15:47:41 +0200

    treti reseni

--- searching.py
+++ searching.py
@@ -30,13 +30,39 @@ def linear_search(field, hledane_cislo):
     slovnik = {"positions":  positions, "count": count}
     return  slovnik
 
+def binary_search(numbers, hledane_cislo):
+    levy_okraj = 0
+    pravy_okraj = len(numbers) - 1
+    while levy_okraj <= pravy_okraj:
+        stred = round((levy_okraj + pravy_okraj) / 2)
+        if numbers[stred] == hledane_cislo:
+            return stred
+        elif numbers[stred] < hledane_cislo:
+            levy_okraj = stred +1
+        else:
+            pravy_okraj = stred -1
+    return None
+    # print(levy_okraj, pravy_okraj, stred)
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
-    sequential_data = read_data("sequential.json", "ordered_numbers")
+    sequential_data = read_data("sequential.json", "unordered_numbers")
     vysledek = linear_search(sequential_data, 14)
+    sequential_data2 = read_data("sequential.json", "ordered_numbers")
+    vysledek2 = binary_search(sequential_data2, 13)
     pass
-    print(sequential_data)
+    print(sequential_data2)
     print(vysledek)
+    print(vysledek2)
 if __name__ == '__main__':
     main()
